cdede568e8b3cbcb7af8d2859c947f9f83cde639
itsPetros <stepanyannn06@gmail.com>
2023-07-13 18:21:40 +0400

    ok

--- threenumbers/code.c
+++ threenumbers/code.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+int main()
+{
+    int a;
+    int b;
+    int c;
+    printf("Enter the first number \n");
+    scanf("%d", &a);
+    printf("Enter the second number \n");
+    scanf("%d", &b);
+    printf("Enter the third number \n");
+    scanf("%d", &c);
+
+    if(((a == b) != c)  || ((a == c) != b) || ((b == c) != a)) { 
+        if ((c > a  || c > b)    )  {
+            printf("%d", c);
+        }
+        else if (b  > a || b > c ) {
+            printf("%d", b);
+        }
+        else if (a > c || a > b ) {
+            printf("%d", a);
+        }
+    }
+    else{
+        printf("Ooops....");
+    }
+    return 0;
+}
