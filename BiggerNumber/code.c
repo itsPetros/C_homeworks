f06a060f5c36673d090f00997644b8a4f72da2cf
itsPetros <stepanyannn06@gmail.com>
2023-07-20 10:16:18 +0400

    ok

--- BiggerNumber/code.c
+++ BiggerNumber/code.c
@@ -9,16 +9,11 @@ int main()
     scanf("%d", &a);
     printf("Enter the second number \n");
     scanf("%d", &b);
-    if ((a >= 48 && a <= 57) || (b >= 48 && b <= 57)) {
         if (a > b) {
             printf("%d is the biggest \n", a);
         }
         else{
             printf("%d is the biggest \n", b);
-        }
-    }
-    else{
-        printf("Invalid digit \n");    
         }
 
     return 0;
